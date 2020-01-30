c8a2b37cf05717270b4146772c65cbdcc21882a1
Calliope Kostopoulou <kelkost@yahoo.gr>
2020-01-30 17:25:19 +0300

    update test_bloom

--- horovod/tensorflow/test_bloom.py
+++ horovod/tensorflow/test_bloom.py
@@ -1,10 +1,24 @@
 import tensorflow as tf
 
+values = tf.constant([10,20])
+print(values.shape)
+print(values.get_shape())
+
+indices = tf.constant([1,2])
+print(indices.shape)
+
 bloom_module = tf.load_op_library('./bloom_compressor_op.so')
 bloom_compressor = bloom_module.bloom_compressor
 
+
+#bloom = bloom_compressor([1, 2], [3, 4])
+bloom = bloom_compressor(values, indices)
+
 with tf.Session() as sess:
-	print(sess.run(bloom_compressor([1, 2], [3, 4])))
+#	print(bloom.get_shape())
+	print(sess.run(bloom))
+        print(bloom.get_shape())
+
 
 # Prints
 #array([[1, 0], [0, 0]], dtype=int32)
