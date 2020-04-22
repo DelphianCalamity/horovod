1557d1543abba84289db9dcfdb923832c9942b22
Calliope Kostopoulou <kelkost@yahoo.gr>
2020-04-22 11:47:04 +0300

    optimizing conflict_sets policy

--- horovod/tensorflow/policies.hpp
+++ horovod/tensorflow/policies.hpp
@@ -7,10 +7,7 @@
 #include "tensorflow/core/framework/op_kernel.h"
 #include "tensorflow/core/framework/tensor.h"
 #include "tensorflow/core/framework/op.h"
-
 #include "../../third_party/bloomfilter/inc/OrdinaryBloomFilter.hpp"
-#include "../../third_party/bloomfilter/inc/MurmurHash.hpp"
-#include "../../third_party/bloomfilter/inc/FnvHash.hpp"
 
 using namespace tensorflow;
 #include <random>
@@ -43,85 +40,121 @@ public:
         return policy_errors;
     }
 
-    static void build_conflict_sets(int N, bloom::OrdinaryBloomFilter<uint32_t>& bloom, std::map<int, std::vector<int>>& conflict_sets) {
+    static void build_conflict_sets(int N, bloom::OrdinaryBloomFilter<uint32_t>& bloom, std::map<int, std::unordered_set<int>>& conflict_sets) {
         // Iterating over the universe and collecting the conflict sets
         uint8_t hash_num = bloom.Get_numHashes();
         for (size_t i=0; i<N; i++) {
             if (bloom.Query(i)) {  // If it is positive
                 for (uint8_t j=0; j<hash_num; j++) {
                     int hash = bloom.Get_Hash(i,j);
-                    std::vector<int>& cs = conflict_sets[hash];
-                    if (std::find(cs.begin(), cs.end(), i) == cs.end()) {
-                        conflict_sets[hash].push_back(i);
-                    }
+//                    std::set<int>& cs = conflict_sets[hash];
+//                    if (std::find(cs.begin(), cs.end(), i) == cs.end()) {
+                    conflict_sets[hash].insert(i);
+//                    }
                 }
             }
         }
     }
 
-    static void transform_and_sort(std::map<int, std::vector<int>>& conflict_sets, std::vector<std::vector<int>>& conflict_sets_ordered) {
+    static void transform_and_sort(std::map<int, std::unordered_set<int>>& conflict_sets, std::vector<std::unordered_set<int>>& conflict_sets_ordered) {
 
-        typedef std::function<std::vector<int>(std::pair<int, std::vector<int>>)> Transformator;
-        Transformator transformator = [](std::pair<int, std::vector<int>> i) {
+        typedef std::function<std::unordered_set<int>(std::pair<int, std::unordered_set<int>>)> Transformator;
+        Transformator transformator = [](std::pair<int, std::unordered_set<int>> i) {
             return i.second;
         };
         std::transform(conflict_sets.begin(), conflict_sets.end(), conflict_sets_ordered.begin(), transformator);
-        std::sort(conflict_sets_ordered.begin(), conflict_sets_ordered.end(), [](const std::vector<int> l, const std::vector<int> r) {
+        std::sort(conflict_sets_ordered.begin(), conflict_sets_ordered.end(), [](const std::unordered_set<int>& l, const std::unordered_set<int>& r) {
             return l.size() < r.size();
         });
     }
 
-    static void choose_indices_from_conflict_sets(int K, int seed, std::vector<std::vector<int>>& conflict_sets_ordered, std::vector<int>& selected_indices) {
-        std::default_random_engine generator;
-        generator.seed(seed);
+//    static void print_map(std::map<int, std::unordered_set<int>>& map) {
+//        for (auto& it: map) {
+//             printf("Key: %d, Values: ", it.first);
+//             for (auto& itt : it.second)
+//                printf("%d, ", itt);
+//             printf("\n");
+//        }
+//    }
+//    static void print_2d_vector(std::vector<std::unordered_set<int>>& vec) {
+//        printf("Conflict Sets:\n");
+//        for (auto& it: vec) {
+//             printf("       {");
+//             for (auto& itt : it) {
+//                printf("%d, ", itt);
+//             }
+//             printf("}\n");
+//        }
+//    }
+//    static void print_vector(std::vector<int>& vec) {
+//        printf("\n[");
+//        int i=0;
+//        for (i = 0; i < vec.size()-1; i++) {
+//            printf("%d, ", (int) vec[i]);
+//        }
+//        printf("%d]\n\n", (int) vec[i]);
+//    }
 
-        int random, idx, left = K;
-        while (left > 0) {                                      // Don't stop until you have selected K positives
-            std::vector<int>& cset = conflict_sets_ordered[0];
-            std::uniform_int_distribution<int> distribution(0, cset.size()-1);
-            random = distribution(generator);
-            idx = cset[random];
-            selected_indices.push_back(idx);
-            left--;
-            // Search the item in all the other conflicts sets and erase it
-            // Then move those conflicts sets at the end of the vector
-            int size = conflict_sets_ordered.size();
-            for (int i=0; i<size; i++) {
-                std::vector<int>& cs = conflict_sets_ordered[i];
-                auto it = std::find(cs.begin(), cs.end(), idx);
-                if (it != cs.end()) {
-                    cs.erase(it);
-                    if (cs.size()) {
-                        conflict_sets_ordered.push_back(cs);
-                    }
-                    conflict_sets_ordered.erase(conflict_sets_ordered.begin()+i);
-                    i--;
-                }
+    static void erase_intersection(std::unordered_set<int>& a, std::unordered_set<int>& b) {
+        std::unordered_set<int>::iterator it;
+        for (it = a.begin(); it != a.end();) {
+            if (b.find(*it) != b.end()) {
+                it = a.erase(it);
+            } else {
+                it++;
             }
         }
+    }
+
+    static void choose_indices_from_conflict_sets(int K, int seed, std::vector<std::unordered_set<int>>& conflict_sets_ordered, std::vector<int>& selected_indices) {
+        std::default_random_engine generator;
+        generator.seed(seed);
+        int random, left = K;
+        std::unordered_set<int> selected_indices_set;
+//print_2d_vector(conflict_sets_ordered);
+        while (left > 0) {             // Don't stop until you have selected K positives
+            for (int i=0; i<conflict_sets_ordered.size() && left>0; i++) {
+                std::unordered_set<int>& cset = conflict_sets_ordered[i];
+                erase_intersection(cset, selected_indices_set);
+                if (cset.size() > 0) {
+                    std::uniform_int_distribution<int> distribution(0, cset.size()-1);
+                    random = distribution(generator);
+                    auto it = std::begin(cset); std::advance(it, random);
+                    selected_indices_set.insert(*it);
+                    cset.erase(it);
+                    left--;
+//printf("\nChoosing: %d\n\n", *it);
+                }
+//print_2d_vector(conflict_sets_ordered);
+//printf("\n\n############################\n\n");
+            }
+        }
+        std::copy(selected_indices_set.begin(), selected_indices_set.end(), std::back_inserter(selected_indices));
         std::sort(selected_indices.begin(), selected_indices.end());
+//print_vector(selected_indices);
     }
 
     static void conflict_sets_policy(int N, int K, int seed, bloom::OrdinaryBloomFilter<uint32_t>& bloom,
                                     std::vector<int>& selected_indices) {
-        std::map<int, std::vector<int>> conflict_sets;
+        std::map<int, std::unordered_set<int>> conflict_sets;
         build_conflict_sets(N, bloom, conflict_sets);
-
+//print_map(conflict_sets);
         // Sort the conflict sets by their size
-        std::vector<std::vector<int>> conflict_sets_ordered;
+        std::vector<std::unordered_set<int>> conflict_sets_ordered;
         conflict_sets_ordered.resize(conflict_sets.size());
         transform_and_sort(conflict_sets, conflict_sets_ordered);
-
         // Collect selected indices
         choose_indices_from_conflict_sets(K, seed, conflict_sets_ordered, selected_indices);
      }
 
+
     static void leftmostK(int N, int K, bloom::OrdinaryBloomFilter<uint32_t>& bloom,
                                     std::vector<int>& selected_indices) {
         // Iterating over the universe and collecting the first K positives
-        for (size_t i=0; i<N; i++) {
+        for (size_t i=0, left=K; i<N && left>0; i++) {
             if (bloom.Query(i)) {  // If it is positive
                 selected_indices.push_back(i);
+                left--;
             }
         }
      }
